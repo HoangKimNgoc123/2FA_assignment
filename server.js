b0c5006dfbf9d9ee96f44f56330bd9a23fd88f7e
HoangKimNgoc123 <hoangkimngoc2810@gmail.com>
2025-02-19 14:13:03 +0700

    continue

---
+++ server.js
@@ -0,0 +1,14 @@
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3100;
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World \n Hoang Kim Ngoc');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
